c910a71f0dfc3d6e0939d3d7171574769db9c14f
rick <technoweenie@gmail.com>
2008-01-08 13:24:06 -0800

    correct route name in authenticated_system

--- generators/authenticated/templates/authenticated_system.rb
+++ generators/authenticated/templates/authenticated_system.rb
@@ -64,7 +64,7 @@ module AuthenticatedSystem
       respond_to do |format|
         format.html do
           store_location
-          redirect_to new_<%= controller_singular_name %>
+          redirect_to new_<%= controller_singular_name %>_path
         end
         format.xml do
           request_http_basic_authentication 'Web Password'
