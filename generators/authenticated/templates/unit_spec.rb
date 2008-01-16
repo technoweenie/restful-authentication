f3931ce924e097ff7e7f44c168ce1d0959407358
rick <technoweenie@gmail.com>
2008-01-16 14:47:00 -0800

    more stateful specs added

--- generators/authenticated/templates/unit_spec.rb
+++ generators/authenticated/templates/unit_spec.rb
@@ -1,18 +1,35 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# Be sure to include AuthenticatedTestHelper in test/test_helper.rb instead.
+# Be sure to include AuthenticatedTestHelper in spec/spec_helper.rb instead.
 # Then, you can remove it from this and the functional test.
 include AuthenticatedTestHelper
 
 describe <%= class_name %> do
   fixtures :<%= table_name %>
-  
-  it 'creates <%= file_name %>' do
-    lambda do
-      <%= file_name %> = create_<%= file_name %>
-      violated "#{<%= file_name %>.errors.full_messages.to_sentence}" if <%= file_name %>.new_record?
-    end.should change(<%= class_name %>, :count).by(1)
-  end
+
+  describe 'being created' do
+    before do
+      @<%= file_name %> = nil
+      @creating_<%= file_name %> = lambda do
+        @<%= file_name %> = create_<%= file_name %>
+        violated "#{@<%= file_name %>.errors.full_messages.to_sentence}" if @<%= file_name %>.new_record?
+      end
+    end
+    
+    it 'increments User#count' do
+      @creating_<%= file_name %>.should change(<%= class_name %>, :count).by(1)
+    end
+<% if options[:include_activation] %>
+    it 'initializes #activation_code' do
+      @creating_<%= file_name %>.call
+      @<%= file_name %>.activation_code.should_not be_nil
+    end
+<% end %><% if options[:stateful] %>
+    it 'starts in pending state' do
+      @creating_<%= file_name %>.call
+      @<%= file_name %>.should be_pending
+    end
+<% end %>  end
 
   it 'requires login' do
     lambda do
@@ -113,18 +130,37 @@ describe <%= class_name %> do
     <%= class_name %>.authenticate('quentin', 'test').should_not == <%= table_name %>(:quentin)
   end
 
-  it 'unsuspends <%= file_name %>' do
-    <%= table_name %>(:quentin).suspend!
-    <%= table_name %>(:quentin).should be_suspended
-    <%= table_name %>(:quentin).unsuspend!
-    <%= table_name %>(:quentin).should be_active
-  end
-
   it 'deletes <%= file_name %>' do
     <%= table_name %>(:quentin).deleted_at.should be_nil
     <%= table_name %>(:quentin).delete!
     <%= table_name %>(:quentin).deleted_at.should_not be_nil
     <%= table_name %>(:quentin).should be_deleted
+  end
+
+  describe "being unsuspended" do
+    fixtures :<%= table_name %>
+
+    before do
+      @<%= file_name %> = <%= table_name %>(:quentin)
+      @<%= file_name %>.suspend!
+    end
+    
+    it 'reverts to active state' do
+      @<%= file_name %>.unsuspend!
+      @<%= file_name %>.should be_active
+    end
+    
+    it 'reverts to passive state if activation_code and activated_at are nil' do
+      <%= class_name %>.update_all :activation_code => nil, :activated_at => nil
+      @<%= file_name %>.reload.unsuspend!
+      @<%= file_name %>.should be_passive
+    end
+    
+    it 'reverts to pending state if activation_code is set and activated_at is nil' do
+      <%= class_name %>.update_all :activation_code => 'foo-bar', :activated_at => nil
+      @<%= file_name %>.reload.unsuspend!
+      @<%= file_name %>.should be_pending
+    end
   end
 <% end %>
 protected
