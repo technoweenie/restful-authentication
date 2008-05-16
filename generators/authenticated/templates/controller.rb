2c84070560fcd15d02bfe687fbff750907abe270
Philip (flip) Kromer <flip@infochimps.org>
2008-05-16 08:58:25 -0500

    * use uniform logout function * avoid calling logged_in? which will auto-log-you-in (safe in the face of   logout! call, but idiot-proof * log in is where reset_session is really needed -- wherever it goes, it has to   be in front of the current_user= call * using uniform remember_cookie functions * made a place to take action on failed login attempt * recycle login and remember_me setting on failed login * nil'ed out the password field in 'new' view

--- generators/authenticated/templates/controller.rb
+++ generators/authenticated/templates/controller.rb
@@ -8,24 +8,41 @@ class <%= controller_class_name %>Controller < ApplicationController
   end
 
   def create
-    self.current_<%= file_name %> = <%= class_name %>.authenticate(params[:login], params[:password])
-    if logged_in?
+    logout_keeping_session!
+    <%= file_name %> = <%= class_name %>.authenticate(params[:login], params[:password])
+    if <%= file_name %> 
+      # Protects against session fixation attacks, causes request forgery
+      # protection if user resubmits an earlier form using back
+      # button. Uncomment if you understand the tradeoffs.
+      # reset_session      
+      self.current_<%= file_name %> = <%= file_name %>
       if params[:remember_me] == "1"
-        current_<%= file_name %>.remember_me unless current_<%= file_name %>.remember_token?
-        cookies[:auth_token] = { :value => self.current_<%= file_name %>.remember_token , :expires => self.current_<%= file_name %>.remember_token_expires_at }
+        make_or_refresh_remember_cookie!
+      else
+        refresh_remember_cookie_if_set!
       end
       redirect_back_or_default('/')
       flash[:notice] = "Logged in successfully"
     else
+      note_failed_signin
+      @login       = params[:login]
+      @remember_me = params[:remember_me]
       render :action => 'new'
     end
   end
 
   def destroy
-    self.current_<%= file_name %>.forget_me if logged_in?
-    cookies.delete :auth_token
-    reset_session
+    logout_killing_session!
     flash[:notice] = "You have been logged out."
     redirect_back_or_default('/')
   end
+
+protected
+
+  # Track failed login attempts
+  def note_failed_signin
+    flash[:error] = "Couldn't log you in as '#{params[:login]}'"
+    logger.warn "Failed login for '#{params[:login]}' from #{request.remote_ip} at #{Time.now.utc}"
+  end
+
 end
