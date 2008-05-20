3e7ec328cdcdf534a635c810c8bde657d80934c9
Philip (flip) Kromer <flip@infochimps.org>
2008-05-20 00:03:36 -0500

    fixed tests: * moved *_test generator source into templates/test * tests work now

---
+++ init.rb
@@ -0,0 +1,3 @@
+require File.dirname(__FILE__) + '/lib/authentication'
+require File.dirname(__FILE__) + '/lib/authentication/by_password'
+require File.dirname(__FILE__) + '/lib/authentication/by_cookie_token'
