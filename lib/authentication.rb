9f75344a291275ce06d6610246174740a8fd959e
Todd Sedano <tsedano@dhcp181.west.cmu.edu>
2008-06-23 14:54:48 -0700

    Added edu to the list of acceptable domains (ie .com, .org, .net)

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -14,7 +14,7 @@ module Authentication
     RE_EMAIL_NAME   = '[\w\.%\+\-]+'                          # what you actually see in practice
     #RE_EMAIL_NAME   = '0-9A-Z!#\$%\&\'\*\+_/=\?^\-`\{|\}~\.' # technically allowed by RFC-2822
     RE_DOMAIN_HEAD  = '(?:[A-Z0-9\-]+\.)+'
-    RE_DOMAIN_TLD   = '(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
+    RE_DOMAIN_TLD   = '(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|jobs|museum)'
     RE_EMAIL_OK     = /\A#{RE_EMAIL_NAME}@#{RE_DOMAIN_HEAD}#{RE_DOMAIN_TLD}\z/i
     MSG_EMAIL_BAD   = "should look like an email address."
     
