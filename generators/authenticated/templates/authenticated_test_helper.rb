61f377d16c805ad608abef638e0fa29a024e846d
Barry Hess <barry@bjhess.com>
2008-11-21 13:29:39 -0600

    Generated AuthenticatedTestHelper#login_as now accepts both symbol for lookup in the fixture as well as an instance of the originating class.

--- generators/authenticated/templates/authenticated_test_helper.rb
+++ generators/authenticated/templates/authenticated_test_helper.rb
@@ -1,7 +1,7 @@
 module AuthenticatedTestHelper
   # Sets the current <%= file_name %> in the session from the <%= file_name %> fixtures.
   def login_as(<%= file_name %>)
-    @request.session[:<%= file_name %>_id] = <%= file_name %> ? <%= table_name %>(<%= file_name %>).id : nil
+    @request.session[:<%= file_name %>_id] = <%= file_name %> ? (<%= file_name %>.is_a?(<%= file_name.camelize %>) ? <%= file_name %>.id : <%= table_name %>(<%= file_name %>).id) : nil
   end
 
   def authorize_as(<%= file_name %>)
