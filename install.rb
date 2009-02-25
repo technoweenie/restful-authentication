7e11db69cac7053b622ed336883c1e90ea6ab1f7
Matt Kern <mkern@artisantechnologies.com>
2009-02-24 22:51:28 -0800

    trying a changed install.rb rather than adding a blank README

--- install.rb
+++ install.rb
@@ -1,1 +1,5 @@
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+if File.exists(File.join(File.dirname(__FILE__), 'README'))
+  puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+else
+  puts IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
+end
