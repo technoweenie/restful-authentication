1ab4f3435f1a22d2cafda2127b9a4bb1c3c4fe9b
Matt Kern <mkern@artisantechnologies.com>
2009-02-24 23:15:20 -0800

    exists? not exist

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
-if File.exists(File.join(File.dirname(__FILE__), 'README'))
+if File.exists?(File.join(File.dirname(__FILE__), 'README'))
   puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 else
   puts IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
