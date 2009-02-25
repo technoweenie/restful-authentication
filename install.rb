8834d9c64d8abda940c9872e6c7784ee39dc0c1a
Matt Kern <mkern@artisantechnologies.com>
2009-02-24 23:42:23 -0800

    Probably best to just remove install.rb since nothing really happens in the install hook. Fixes issue #36, the plugin not found issue.

--- install.rb
+++
@@ -1,5 +0,0 @@
-if File.exists?(File.join(File.dirname(__FILE__), 'README'))
-  puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-else
-  puts IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
-end
